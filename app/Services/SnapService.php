b8162de85735bc6063163a2a43cf0e1491923687
Pieter Lelaona <pieter@rebelworks.co>
2016-11-20 02:56:38 +0700

    add snap service

--- app/Services/SnapService.php
+++ app/Services/SnapService.php
@@ -2,11 +2,53 @@
 
 namespace App\Services;
 
+use App\Exceptions\Services\SnapServiceException;
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+
 class SnapService
 {
 
-    public function bill($request)
+    const IMAGE_FIELD_NAME = 'snap_images';
+
+    public function handle(Request $request, $type)
     {
 
     }
+
+    protected function imageProcess(Request $request)
+    {
+        if ($this->countOfImage($request) <= 1) {
+            throw new SnapServiceException('Can not do the process');
+        }
+
+        foreach ($request->file(self::IMAGE_FIELD_NAME) as  $image) {
+
+        }
+    }
+
+    protected function audioProcess(Request $request)
+    {
+
+    }
+
+    protected function tagProcess(Request $request)
+    {
+
+    }
+
+    private function countOfImage(Request $request)
+    {
+        $images = ($request->has(self::IMAGE_FIELD_NAME) && null !== $request->file(self::IMAGE_FIELD_NAME)) ?
+            $request->file(self::IMAGE_FIELD_NAME) :
+            [];
+
+        return count($images);
+    }
+
+    private function uploadProcess(UploadedFile $image)
+    {
+
+    }
+
 }
