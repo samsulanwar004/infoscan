ffa6f93626bc908284cd20a327afd32257cb9366
Samsul Anwar <samsul@rebelworks.co>
2016-11-25 11:22:30 +0700

    fixed loading screen in create & edit merchant

--- resources/views/merchants/create.blade.php
+++ resources/views/merchants/create.blade.php
@@ -137,7 +137,7 @@
 
     function myLoading() {
         $('#loading').addClass('overlay');
-        document.getElementById("loading").innerHTML = '<i class="fa fa-refresh fa-spin"></i>';
+        document.getElementById("loading").innerHTML = '<i class="fa fa-spinner fa-spin" style="font-size:50px; position: fixed;"></i>';
     }
 </script>
 @endsection
