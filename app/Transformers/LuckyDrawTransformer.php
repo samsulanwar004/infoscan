50081d9490e23e747d168a7238ffe3baf9449c35
Samsul Anwar <samsul@rebelworks.co>
2016-12-14 11:41:42 +0700

    add api for get lucky draw list

---
+++ app/Transformers/LuckyDrawTransformer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Transformers;
+
+use League\Fractal\TransformerAbstract;
+
+class LuckyDrawTransformer extends TransformerAbstract
+{
+	public function transform($lucky)
+    {
+        return [
+            'id'   => $lucky->id,
+            'code'   => $lucky->luckydraw_code,
+            'title'   => $lucky->title,
+            'description'  => $lucky->description,
+            'image' => $lucky->image,
+        ];
+    }
+}
