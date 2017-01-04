b58c1e52996abd07487b0043337253cc45ff72e6
Samsul Anwar <samsul@rebelworks.co>
2017-01-04 18:14:16 +0700

    update modal for approve

--- resources/views/snaps/edit.blade.php
+++ resources/views/snaps/edit.blade.php
@@ -1,59 +1,76 @@
-<form id="modalForm" action="{{ admin_route_url('snaps.update', ['id' => $snapFile->mode_type]) }}"  method="POST">
+<form id="modalForm" action="{{ admin_route_url('snaps.update', ['id' => $snap->id]) }}"  method="POST">
     {{ csrf_field() }}
     {{ method_field('PUT') }}
     <div class="modal-header">
         <a class="close" data-dismiss="modal">&times;</a>
-        <h4><i class="fa fa-file-o fa-btn"></i> <span class="action-title">Snap </span> File</h4>
+        <h4><i class="fa fa-file-o fa-btn"></i> <span class="action-title">Snap </span> Confirmation</h4>
     </div>
     <div class="modal-body">
-        <div class="form-horizontal">
-            @if ($snapFile->mode_type === 'input')
-                <div class="col-md-6">
-                    <img src="{{$snapFile->file_path}}" alt="" class="margin img-responsive">
+        <div class="row">
+            <div class="col-xs-12">
+              <h2 class="page-header">
+                <i class="fa fa-globe"></i> AdminLTE, Inc.
+                <small class="pull-right">Date: {{ date_format(date_create($snap->created_at), 'M, d Y') }}</small>
+              </h2>
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- info row -->
+          <div class="row invoice-info">                
+            <div class="col-sm-4 invoice-col">
+              <b>Request Code :</b> {{ $snap->request_code }}<br>                      
+              <b>Snap Type :</b> {{ strtoupper($snap->snap_type) }}<br>
+              <b>Mode Type :</b> {{ strtoupper($snap->mode_type) }}<br>
+              <b>Member Name :</b> {{ $snap->member->name }}<br>
+            </div>
+            <!-- /.col -->
+            <div class="col-sm-4 invoice-col">
+                <div class="checkbox">
+                    <label>
+                        <input name="approve" {{ (bool)$snap->approved_by ? 'checked' : '' }} type="checkbox">
+                        Approve
+                    </label>
                 </div>
-                <div class="col-md-6" style="overflow-y:scroll;max-height: 500px;">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th width="50">No</th>
-                                <th width="300">Item</th>
-                                <th width="100">Qty</th>
-                                <th width="200" class="text-right">Price</th>
-                            </tr>
-                        </thead>
+                <div class="checkbox">
+                    <label>
+                        <input name="check" {{ (bool)$snap->check_by ? 'checked' : '' }} type="checkbox">
+                        Check
+                    </label>
+                </div>
+            </div>
+          </div>
+          <!-- /.row -->
 
-                        <tbody id="tags">
-                            @foreach($snapFile->tag as $tag)
-                                <tr>
-                                    <td>#</td>
-                                    <td width="300"><input type="text" name="tag[name][]" class="form-control input-sm tag-name" value="{{ $tag->name }}" placeholder="Product Name" required="required"></td>
-                                    <td width="100"><input type="number" name="tag[qty][]" class="form-control input-sm" value="{{ $tag->quantity }}" placeholder="QTY" required="required"></td>
-                                    <td width="200" class="text-right"><input type="number" name="tag[total][]" class="form-control input-sm" value="{{ $tag->total_price }}" placeholder="Total Price" required="required"></td>
-                                    <input type="hidden" name="tag[id][]" value="{{ $tag->id }}">
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            @elseif ($snapFile->mode_type === 'tags')
-                ini tags
-            @elseif ($snapFile->mode_type === 'audio')
-                <audio controls>
-                  <source src="{{$snapFile->file_path}}" type="audio/mpeg">
-                    Your browser does not support the audio element.
-                </audio>
-            @endif
-        </div>
+          <!-- Table row -->
+          <div class="row">
+            <div class="col-xs-12 table-responsive" style="overflow-y:scroll;max-height: 500px;">
+              <table class="table table-striped">
+                <thead>
+                    <tr>
+                      <th>File Code</th>
+                      <th>Score</th>
+                    </tr>
+                </thead>
+                <tbody>
+                @foreach($snap->files as $file)
+                    <tr>
+                      <td>{{ $file->file_code }}</td>
+                      <td>{{ $file->recognition_score }}</td>
+                    </tr>   
+                @endforeach                     
+                </tbody>
+              </table>
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
     </div>
     <div class="modal-footer">
         <div class="button-container">
             <a class="btn btn-link" data-dismiss="modal">Close</a>
             <button class="btn btn-primary submit-to-server">
-                <i class="fa fa-save fa-btn"></i> <span class="ladda-label">Save Item</span>
+                <i class="fa fa-save fa-btn"></i> <span class="ladda-label">Save</span>
             </button>
-            <a class="btn btn-primary" id="add">
-                <i class="fa fa-plus fa-btn"></i>Create New
-            </a>
             <div class="la-ball-fall">
                 <div></div>
                 <div></div>
@@ -63,30 +80,3 @@
     </div>
 </form>
 
-<script type="text/javascript">
-    $("modalForm").ready(function () {
-        
-        $('form').on('focus', 'input[type=number]', function (e) {
-          $(this).on('mousewheel.disableScroll', function (e) {
-            e.preventDefault()
-          })
-        });
-        $('form').on('blur', 'input[type=number]', function (e) {
-          $(this).off('mousewheel.disableScroll')
-        });
-
-        $('a#add').on('click', function (e) {
-            e.preventDefault();
-            var countOfTextbox = $('.tag-name').length; console.log(countOfTextbox);
-
-            if(countOfTextbox >= 25) {
-                $(this).attr('disabled', 'disabled');
-                return;
-            }
-
-            $('tbody#tags').append('<tr><td>#</td><td width="300"><input type="text" name="newtag[name][]" class="form-control input-sm tag-name" placeholder="Product Name" required="required"></td><td width="100"><input type="number" name="newtag[qty][]" class="form-control input-sm" placeholder="QTY" required="required"></td><td width="200" class="text-right"><input type="number" name="newtag[total][]" class="form-control input-sm" placeholder="Total Price" required="required"><input type="hidden" name="newtag[fileId][]" value="{{ $snapFile->id }}"></td></tr>');
-        });
-
-    });
-
-</script>
