6775c32eabca3a9bf4d3757c57e0162f09534579
Pieter Lelaona <pieter@rebelworks.co>
2017-01-11 13:54:55 +0700

    change node version

--- .ebextensions/bin/install-nodejs.sh
+++ .ebextensions/bin/install-nodejs.sh
@@ -26,8 +26,9 @@ install_node () {
         #echo 'nodejs install, add more processing if needed' > /dev/null
         yum rm nodejs
         sudo rm -f /usr/bin/node
-        sudo yum install nodejs --enablerepo=epel-testing
     else
+        yum rm nodejs
+        sudo rm -f /usr/bin/node
         curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
         yum install nodejs
     fi
