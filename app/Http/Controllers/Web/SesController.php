d56719b7a89591ae006cf4ef5433d8286b520584
Pieter Lelaona <pieter@rebelworks.co>
2017-01-12 18:49:19 +0700

    fix bugs on unique validation SES

--- app/Http/Controllers/Web/SesController.php
+++ app/Http/Controllers/Web/SesController.php
@@ -33,7 +33,7 @@ class SesController extends AdminController
     public function store(Request $request)
     {
         $this->validate($request, [
-            'code' => 'required|string|unique:code',
+            'code' => 'required|string|unique:socio_economic_status,code',
             'range_start' => 'required|numeric|different:range_end|min:0',
             'range_end' => 'required|numeric|min:0'
         ]);
@@ -54,7 +54,7 @@ class SesController extends AdminController
     public function update(Request $request, $id)
     {
         $this->validate($request, [
-            'code' => 'required|string|unique:code',
+            'code' => 'required|string|unique:socio_economic_status,code',
             'range_start' => 'required|numeric|different:range_end|min:0',
             'range_end' => 'required|numeric|min:0'
         ]);
