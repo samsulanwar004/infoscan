bd3f2812f03ff7cda9ef994d05b679de7e0fa121
Samsul Anwar <samsul@rebelworks.co>
2017-01-12 18:05:51 +0700

    fixing approved

--- resources/views/snaps/show.blade.php
+++ resources/views/snaps/show.blade.php
@@ -49,21 +49,12 @@
 
                     </div>
                         <div class="col-md-4">
-                            @if($snap->approved_by == null)
                             <a href="{{ admin_route_url('snaps.edit', ['id' => $snap->id]) }}" class="btn btn-success btn-block btn-lg"
                                 data-toggle="modal"
                                 data-target="#"
                                 modal-size="modal-lg" 
                                 title="Edit">
                                 <i class="fa fa-check-circle-o fa-btn"></i>Approve This Content</a>  
-                            @else
-                                <a href="{{ admin_route_url('snaps.edit', ['id' => $snap->id]) }}" class="btn btn-danger btn-block btn-lg"
-                                data-toggle="modal"
-                                data-target="#"
-                                modal-size="modal-lg" 
-                                title="Edit">
-                                <i class="fa fa-check-circle-o fa-btn"></i>This Content Approved</a>
-                            @endif
                             <div class="snaps-detail">
                                 @include('snaps.show_detail', ['snap' => $snap])
                             </div>   
