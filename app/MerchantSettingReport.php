8aae6aca972793df877fcb512008669e2e694e51
Pieter Lelaona <pieter@rebelworks.co>
2017-01-18 23:20:50 +0700

    removing the stupid things

--- app/MerchantSettingReport.php
+++ app/MerchantSettingReport.php
@@ -4,13 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Rebel\Component\Rbac\Traits\HasRoles;
 
-class MerchantSettingReport extends Model {
-    use Notifiable, HasRoles, SoftDeletes;
+class MerchantSettingReport extends Model
+{
+    use SoftDeletes;
+
     protected $table = 'report_templates';
+
     protected $fillable = [
         'name',
         'content',
--- database/migrations/2017_01_18_112419_create_user_setting_table.php
+++ database/migrations/2017_01_18_112419_create_user_setting_table.php
@@ -14,19 +14,19 @@ class CreateUserSettingTable extends Migration
     public function up()
     {
         Schema::create('merchant_users_report_setting', function (Blueprint $table) {
-        $table->increments('id');
-        $table->unsignedInteger('report_template_id');
-        $table->unsignedInteger('merchant_user_id');
-        $table->timestamps();
-        $table->softDeletes();
+            $table->increments('id');
+            $table->unsignedInteger('report_template_id');
+            $table->unsignedInteger('merchant_user_id');
+            $table->timestamps();
+            $table->softDeletes();
 
-        $table->foreign('report_template_id', 'FK_report_template_id_in_murs')
-              ->references('id')
-              ->on('report_templates');
-        $table->foreign('merchant_user_id', 'FK_merchant_user_id_in_murs')
-              ->references('merchant_id')
-              ->on('merchant_users');
-    });   
+            $table->foreign('report_template_id', 'FK_report_template_id_in_murs')
+                  ->references('id')
+                  ->on('report_templates');
+            $table->foreign('merchant_user_id', 'FK_merchant_user_id_in_murs')
+                  ->references('merchant_id')
+                  ->on('merchant_users');
+        });
     }
 
     /**
