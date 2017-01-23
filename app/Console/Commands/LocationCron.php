63d76122e5c40eb4b8fe85dc853c82beb2116de2
Samsul Anwar <samsul@rebelworks.co>
2017-01-23 22:08:24 +0700

    bugs fixing get data address from google map

--- app/Console/Commands/LocationCron.php
+++ app/Console/Commands/LocationCron.php
@@ -50,9 +50,9 @@ class LocationCron extends Command
             if ($location->status == "OK") {
                 $s = $snapService->getSnapByid($snap->id);
                 $s->location = $location->results[0]->formatted_address;
-                $s->outlet_city = $address[$length-4]->long_name;
-                $s->outlet_province = $address[$length-3]->long_name;
-                $s->outlet_zip_code = $address[$length-1]->long_name;
+                $s->outlet_city = in_array("administrative_area_level_2", $address[$length-4]->types) ? $address[$length-4]->long_name : null;
+                $s->outlet_province = in_array("administrative_area_level_1", $address[$length-3]->types) ? $address[$length-3]->long_name : null;
+                $s->outlet_zip_code = in_array("postal_code", $address[$length-1]->types) ? $address[$length-1]->long_name : null;
                 $s->update();
             }
             
