c44fc39b853309331eaf6895bb2d4022bdf4cd25
Pieter Lelaona <pieter@rebelworks.co>
2017-01-25 15:54:51 +0700

    change response key for member name and member email

--- app/Transformers/MemberTransformer.php
+++ app/Transformers/MemberTransformer.php
@@ -11,9 +11,8 @@ class MemberTransformer extends TransformerAbstract
     	$backAccount = is_null($member->bank_account) ?:decrypt($member->bank_account);
 
         return [
-            //'code'   => $member->member_code,
-            'member_name'   => $member->name,
-            'member_email'  => $member->email,
+            'name'   => $member->name,
+            'email'  => $member->email,
             'avatar' => $member->avatar,
             'bank_account' => null === $backAccount ?:$backAccount['bank_account'],
             'account_number' => null === $backAccount ?:substr($backAccount['account_number'], 0, 3) . '-xxx-xxxx',
