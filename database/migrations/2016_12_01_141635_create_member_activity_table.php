7b7816c7760bae9b5b104a2b7754bbd811b28651
Pieter Lelaona <pieter@rebelworks.co>
2017-03-02 21:27:17 +0700

    add length of member code

--- database/migrations/2016_12_01_141635_create_member_activity_table.php
+++ database/migrations/2016_12_01_141635_create_member_activity_table.php
@@ -15,7 +15,7 @@ class CreateMemberActivityTable extends Migration
     {
         Schema::create('member_activities', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('member_code', 10)->index();
+            $table->string('member_code', 50)->index();
             $table->string('action_in', 50)->index();
             $table->text('description')->nullable();
             $table->timestamps();
