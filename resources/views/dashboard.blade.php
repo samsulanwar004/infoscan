5e302427e80332497d6d3e483c127431f62daaf3
Dharma Saputra <mail.dharma.saputra@gmail.com>
2017-07-04 17:57:30 +0700

    add sample chart view

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,18 +11,149 @@
             <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
             <li><a href="#">Examples</a></li>
             <li class="active">Blank page</li>
-        </ol> -->
+            </ol> -->
     </section>
 
     <!-- Main content -->
     <section class="content">
-
-      <div class="row">
-        <div class="col-md-12">
-          Welcome to GoJago admin Panel
+        <div class="col-md-6">
+            <div class="box box-success">
+                <div class="box-header with-border">
+                    <div class="col-md-6">
+                            <h3 class="box-title">Active Users</h3>
+                    </div>
+                    <div class="col-md-3">
+                        <select id="period" name="period" class="form-control">
+                            <option value="daily">Daily</option>
+                            <option value="weekly">Weekly</option>
+                            <option value="monthly">Monthly</option>
+                            <option value="yearly">Yearly</option>
+                        </select> 
+                    </div>
+                   
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+                    </div>
+                </div>
+                <div class="box-body">
+                    <div class="chart">
+                        <canvas id="barChart" style="height: 230px; width: 511px;" width="511" height="230"></canvas>
+                    </div>
+                </div>
+                <!-- /.box-body -->
+            </div> 
         </div>
-      </div>
-
+       
     </section>
     <!-- /.content -->
+@endsection
+
+@section('footer_scripts')
+    <script src="{{ asset('js/Chart.bundle.min.js') }}">
+    </script>
+    <script>
+        $(function () {
+
+            var areaChartData = {
+                labels  : ['Senin', 'Selasa',  'Rabu', 'Kamis', 'Jumat', 'Minggu'],
+                datasets: [
+                    {
+                        label               : 'New Users',
+                        fillColor           : 'rgba(210, 214, 222, 1)',
+                        strokeColor         : 'rgba(210, 214, 222, 1)',
+                        pointColor          : 'rgba(210, 214, 222, 1)',
+                        pointStrokeColor    : '#c1c7d1',
+                        pointHighlightFill  : '#fff',
+                        pointHighlightStroke: 'rgba(220,220,220,1)',
+                        data                : [65, 59, 80, 81, 56, 55, 40]
+                    },
+                    {
+                        label               : 'Snap',
+                        fillColor           : 'rgba(210, 214, 222, 1)',
+                        strokeColor         : 'rgba(210, 214, 222, 1)',
+                        pointColor          : 'rgba(210, 214, 222, 1)',
+                        pointStrokeColor    : '#c1c7d1',
+                        pointHighlightFill  : '#fff',
+                        pointHighlightStroke: 'rgba(220,220,220,1)',
+                        data                : [66, 79, 88, 88, 26, 55, 60]
+                    },
+            
+                    {
+                        label               : 'Receipts',
+                        fillColor           : 'rgba(210, 214, 222, 1)',
+                        strokeColor         : 'rgba(210, 214, 222, 1)',
+                        pointColor          : 'rgba(210, 214, 222, 1)',
+                        pointStrokeColor    : '#c1c7d1',
+                        pointHighlightFill  : '#fff',
+                        pointHighlightStroke: 'rgba(220,220,220,1)',
+                        data                : [65, 59, 80, 81, 56, 55, 40]
+                    },
+                    {
+                        label               : 'Warung',
+                        fillColor           : 'rgba(210, 214, 222, 1)',
+                        strokeColor         : 'rgba(210, 214, 222, 1)',
+                        pointColor          : 'rgba(210, 214, 222, 1)',
+                        pointStrokeColor    : '#c1c7d1',
+                        pointHighlightFill  : '#fff',
+                        pointHighlightStroke: 'rgba(220,220,220,1)',
+                        data                : [66, 79, 88, 88, 26, 55, 60]
+                    },
+                    {
+                        label               : 'Nota Tulis',
+                        fillColor           : 'rgba(210, 214, 222, 1)',
+                        strokeColor         : 'rgba(210, 214, 222, 1)',
+                        pointColor          : 'rgba(210, 214, 222, 1)',
+                        pointStrokeColor    : '#c1c7d1',
+                        pointHighlightFill  : '#fff',
+                        pointHighlightStroke: 'rgba(220,220,220,1)',
+                        data                : [65, 59, 80, 81, 56, 55, 40]
+                    },
+                    
+                ]
+            }
+
+            //-------------
+            //- BAR CHART -
+            //-------------
+            var barChartCanvas                   = $('#barChart').get(0).getContext('2d')
+            var barChartData                     = areaChartData
+            var barChartOptions                  = {
+                //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
+                scaleBeginAtZero        : true,
+                //Boolean - Whether grid lines are shown across the chart
+                scaleShowGridLines      : true,
+                //String - Colour of the grid lines
+                scaleGridLineColor      : 'rgba(0,0,0,.05)',
+                //Number - Width of the grid lines
+                scaleGridLineWidth      : 1,
+                //Boolean - Whether to show horizontal lines (except X axis)
+                scaleShowHorizontalLines: true,
+                //Boolean - Whether to show vertical lines (except Y axis)
+                scaleShowVerticalLines  : true,
+                //Boolean - If there is a stroke on each bar
+                barShowStroke           : true,
+                //Number - Pixel width of the bar stroke
+                barStrokeWidth          : 2,
+                //Number - Spacing between each of the X value sets
+                barValueSpacing         : 5,
+                //Number - Spacing between data sets within X values
+                barDatasetSpacing       : 1,
+                //String - A legend template
+                legendTemplate          : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
+                //Boolean - whether to make the chart responsive
+                responsive              : true,
+                maintainAspectRatio     : true
+            }
+
+            barChartOptions.datasetFill = false
+            barChart = new Chart(barChartCanvas, {
+                type: 'bar',
+                data: barChartData,
+                options: barChartOptions
+
+            })
+        });
+    </script>
 @endsection
