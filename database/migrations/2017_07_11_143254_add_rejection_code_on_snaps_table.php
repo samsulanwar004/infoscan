e8fc31f758e7652aef1c5a7204a0ce0a7ef4cd24
Dharma Saputra <mail.dharma.saputra@gmail.com>
2017-07-11 14:49:53 +0700

    [UPDATE] Migration add rejection_code field on snaps table

---
+++ database/migrations/2017_07_11_143254_add_rejection_code_on_snaps_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRejectionCodeOnSnapsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('snaps', function (Blueprint $table) {
+            $table->string('rejection_code')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('snaps', function (Blueprint $table) {
+            $table->dropColumn('rejection_code');
+        });
+    }
+}
